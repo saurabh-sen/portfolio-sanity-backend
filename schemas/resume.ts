afba10c0b10275e51e25924a02d8d9d993707b5c
Saurabh Sen <sensaurabh1212@gmail.com>
2022-12-15 13:16:54 +0530

    final commit

---
+++ schemas/resume.ts
@@ -0,0 +1,13 @@
+export default{
+    name:'resume',
+    title:'Resume Link',
+    type: 'document',
+    fields:[
+        {
+            name:'resumeUrl',
+            title:'resume',
+            type: 'string',
+        },
+        
+    ]
+}
